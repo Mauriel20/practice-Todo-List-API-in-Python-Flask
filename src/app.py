781b67647772b844e3af54fe2037a5d1b78d64f5
Mauriel <josueazofeifajosue@gmail.com>
2021-02-23 22:16:10 +0000

    My-work

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request, json
+
 app = Flask(__name__)
 
 @app.route('/todos', methods=['GET'])
@@ -9,18 +8,31 @@ def hello_world():
 
 todos = [
     { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False },
+    { "label": "My third task", "done": False },
+
 
 ]
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    decoded_object = request.get_json()
-    todos.append(decoded_object) #Inserta al final del array
+    # decoded_object = request.get_json()
+    # todos.append(decoded_object) #Inserta al final del array
+    # return jsonify(todos) , 200
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < len(todos):
+        todos.pop(position)
+    else:
+        raise Exception("sorry, no numbers below zero")
     return jsonify(todos) , 200
-
-
-
-
+   
 
  
 # These two lines should always be at the end of your app.py file.
